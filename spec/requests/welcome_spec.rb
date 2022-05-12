52a3947f507692343fc71bc77fa17f9a260025df
kabakabakaba <66195723+kabakabakaba@users.noreply.github.com>
2022-05-12 13:03:56 +0900

    testが失敗するテスト

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Welcomes", type: :request do
   describe "GET /index" do
     it 'リクエストが成功すること' do
-      get "/"
+      get "/s"
       assert_response :success
     end
   end
