0eedfc40dd490b7f97efb7ca7caae11c3c7c877b
kabakabakaba <66195723+kabakabakaba@users.noreply.github.com>
2022-06-03 01:35:08 +0900

    [fix]タイムゾーンをasia/tokyoからtokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Myrecipe
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
-    config.time_zone = 'Asia/Tokyo'
+    config.time_zone = 'Tokyo'
   end
 end
