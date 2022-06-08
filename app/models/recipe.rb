6b7acfa669fbeb323eb8de6dd8ccbfa74612c750
kabakabakaba <66195723+kabakabakaba@users.noreply.github.com>
2022-06-09 01:03:50 +0900

    [add]recipe.rb Model テスト追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: { in: 1..50 }
   validates :text, length: { maximum: 300 }
+  validates :user_id, presence: true
 end
