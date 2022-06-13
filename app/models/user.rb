40771dee83e6820c878e3dd564a83f00a513cedd
kabakabakaba <66195723+kabakabakaba@users.noreply.github.com>
2022-06-13 10:03:15 +0900

    [add]user.rb "has_many :recipes, dependent: :destroy"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :recipes, dependent: :destroy
   # Include default devise modules. Others available are:
   # :lockable, and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable, :timeoutable
