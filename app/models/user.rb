11311e56328787921721283f905f2790a37cd9dc
kabakabakaba <66195723+kabakabakaba@users.noreply.github.com>
2022-06-16 03:18:10 +0900

    [add]Userにユーザー名のカラムを追加 ユーザー名のカラムを追加し、登録変更できるようにした。 バリデーションも追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable, :timeoutable
+
+  validates :name, presence: true, length: { in: 5..15 }, format: { with: /\A[a-z][a-z0-9]+\z/i, message: 'は最初の文字は半角英字、その後は半角英数を使って下さい' }
 end
